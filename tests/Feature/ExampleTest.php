f2e5abe10e65b70f5ca7e7c8b126e01e8a2c5a38
Israel Zepeda <israel@xencar.mx>
2026-07-21 18:00:31 -0500

    test example2 npm axios

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -6,9 +6,7 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * Guests should be redirected to login.
-     */
+   
     public function test_guests_are_redirected_to_login(): void
     {
         $response = $this->get('/');
