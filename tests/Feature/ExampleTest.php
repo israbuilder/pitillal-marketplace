d2c937d6e94e879ef38f53f0e22ae3b14f071a80
Israel Zepeda <israel@xencar.mx>
2026-07-23 11:08:25 -0500

    no vite test landing

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -6,11 +6,17 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-   
+   public function test_landing_page_is_accessible(): void
+{
+    
+    $response = $this->get('/');
+
+    $response->assertOk();
+}
     public function test_guests_are_redirected_to_login(): void
     {
-        $this->withoutVite();
-        $response = $this->get('/');
+       
+        $response = $this->get('/app');
 
         $response->assertRedirect(route('login'));
     }
